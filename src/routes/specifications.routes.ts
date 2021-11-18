85f586d73428796830d1629ebcc38ab6aadc72f9
Luís Henrique de Castro Corrêa <lhccorrea@hotmail.com>
2021-11-18 08:13:36 -0300

    Injecao de dependencias

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+
+const specificationsRoutes = Router();
+
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post("/", (request, response) => {
+  return createSpecificationController.handle(request, response);
+});
+
+export { specificationsRoutes };
